eb777dcf080d69fc4249903c1bb35b6a16bc68c5
Sam Kreter <sakfy6@mail.missouri.edu>
2014-09-26 01:09:53 -0500

    fixed modal problem

--- lab4/views/insert.php
+++ lab4/views/insert.php
@@ -1,15 +1,19 @@
 <form method="POST" action="index.php">
-				<input type="hidden" name="action" value="save_insert">
-				Enter data for the city to be added: <br>
-				<table border="1">
-				<tbody><tr><td>Name</td><td><input type="text" name="name"></td></tr>
-				<tr><td>Country Code</td><td><select name="country_code">
-					<option value="BHS">Bahamas</option>
-					<option value="BHR">Bahrain</option>
-				</select></td></tr>
-				<tr><td>District</td><td><input type="text" name="district"></td></tr>
-				<tr><td>Population</td><td><input type="text" name="population"></td></tr>
-				</tbody></table>
-				<input type="submit" value="Save">
-				<input type="button" value="Cancel"class="close-reveal-modal">
+				<div class="row">
+					<input type="hidden" name="action" value="save_insert">
+					Enter data for the city to be added: <br>
+					<table border="1">
+					<tbody><tr><td>Name</td><td><input type="text" name="name"></td></tr>
+					<tr><td>Country Code</td><td><select name="country_code">
+						<option value="BHS">Bahamas</option>
+						<option value="BHR">Bahrain</option>
+					</select></td></tr>
+					<tr><td>District</td><td><input type="text" name="district"></td></tr>
+					<tr><td>Population</td><td><input type="text" name="population"></td></tr>
+					</tbody></table>
+				</div>
+				<div class="row left">
+					<div><input type="submit" class="button"value="Save"></div>
+					<a class="close-reveal-modal">&#215;</a>
+				</div>
 			</form>

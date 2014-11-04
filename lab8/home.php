d5ea9b31a28b5031268eccd5056bb8aa9c21035b
samkreter <sakfy6@mail.missouri.edu>
2014-11-04 16:43:41 -0600

    fixed https bug

--- lab8/home.php
+++ lab8/home.php
@@ -4,7 +4,7 @@ if($_SESSION['loggedin'] !== true){
   header("location: index.php");
 }
 if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
-  $redirect = "https//".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+  $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
   header("location: $redirect");
 }
 
--- lab8/index.php
+++ lab8/index.php
@@ -1,6 +1,6 @@
 <?php require("indexlogic.php");
   if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
-    $redirect = "/https//".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    $redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     header("location: $redirect");
   }
   session_start();

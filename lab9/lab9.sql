1122744894b60b9b344cb549bb26406a3a55eac1
Sam Kreter <sakfy6@mail.missouri.edu>
2014-11-11 19:39:05 -0600

    finished query 2

--- lab9/lab9.sql
+++ lab9/lab9.sql
@@ -2,3 +2,9 @@
 SELECT name10 AS name FROM tl_2010_us_state10
 WHERE ST_Intersects(coords,
   ST_SetSRID(ST_MakeBox2D(ST_Point(-110, 35), ST_Point(-109, 36)), 4326));
+
+#Query2
+SELECT stusps10 AS USPS, name10 AS name
+FROM tl_2010_us_state10 WHERE ST_Touches(coords,
+  (SELECT coords FROM tl_2010_us_state10
+    WHERE stusps10 IN ('NC'))) ORDER BY name ASC;

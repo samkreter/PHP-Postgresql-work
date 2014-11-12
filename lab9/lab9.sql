ecbb61a9c835c1edc84b5f5f0b5004a981befe90
samkreter <sakfy6@mail.missouri.edu>
2014-11-11 21:51:29 -0600

    finial testing for project 9

--- lab9/lab9.sql
+++ lab9/lab9.sql
@@ -1,28 +1,28 @@
-#Query1
+--Query1
 SELECT name10 AS name FROM tl_2010_us_state10
 WHERE ST_Intersects(coords,
   ST_SetSRID(ST_MakeBox2D(ST_Point(-110, 35), ST_Point(-109, 36)), 4326));
 
-#Query2
+--Query2
 SELECT stusps10 AS USPS, name10 AS name
 FROM tl_2010_us_state10 WHERE ST_Touches(coords,
   (SELECT coords FROM tl_2010_us_state10
     WHERE stusps10 IN ('NC'))) ORDER BY name ASC;
-#Query3
+--Query3
 SELECT name10 AS name FROM tl_2010_us_uac10
 WHERE ST_Within(coords,
   (SELECT coords FROM tl_2010_us_state10
     WHERE stusps10 IN ('CO'))) ORDER BY name ASC;
-#Query4
+--Query4
 SELECT name10 AS name, ((aland10 + awater10)*.001) AS area
 FROM tl_2010_us_uac10 WHERE ST_Overlaps(coords,
   (SELECT coords FROM tl_2010_us_state10
     WHERE stusps10 IN ('PA'))) ORDER BY area DESC;
-#Query5
+--Query5
 SELECT one.name10 AS nameOne, two.name10 AS nameTwo
 FROM tl_2010_us_uac10 AS one, tl_2010_us_uac10 AS two
 WHERE ST_Intersects(one.coords, two.coords) AND  one.gid != two.gid AND one.gid < two.gid;
-#Query6
+--Query6
 SELECT u.name10, COUNT(ST_Intersects(s.coords,u.coords)) AS statesInterCount FROM tl_2010_us_uac10 AS u CROSS JOIN tl_2010_us_state10 AS s
 WHERE ST_Intersects(s.coords,u.coords)
 GROUP BY u.gid

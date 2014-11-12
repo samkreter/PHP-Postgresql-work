84b9e94e412caef01c78900e0ee7dcaa410fa7d3
Sam Kreter <sakfy6@mail.missouri.edu>
2014-11-11 19:46:44 -0600

    finished query 5

--- lab9/lab9.sql
+++ lab9/lab9.sql
@@ -14,7 +14,11 @@ WHERE ST_Within(coords,
   (SELECT coords FROM tl_2010_us_state10
     WHERE stusps10 IN ('CO'))) ORDER BY name ASC;
 #Query4
-SELECT name10 AS name, ((aland10 + awater10)*.001) AS area 
+SELECT name10 AS name, ((aland10 + awater10)*.001) AS area
 FROM tl_2010_us_uac10 WHERE ST_Overlaps(coords,
   (SELECT coords FROM tl_2010_us_state10
     WHERE stusps10 IN ('PA'))) ORDER BY area DESC;
+#Query5
+SELECT one.name10 AS nameOne, two.name10 AS nameTwo
+FROM tl_2010_us_uac10 AS one, tl_2010_us_uac10 AS two
+WHERE ST_Intersects(one.coords, two.coords) AND  one.gid != two.gid AND one.gid < two.gid; 

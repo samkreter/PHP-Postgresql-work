0b12177f2fae11a46494bdc7c3cf92f4ca04e296
Sam Kreter <sakfy6@mail.missouri.edu>
2014-11-16 14:57:54 -0600

    started project 10

---
+++ lab10/lab10.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS lab10 CASCADE;
+
+CREATE SCHEMA lab10;
+
+SET search_path = lab10;
+
+CREATE TABLE group_standing(
+  team varchar(25) NOT NULL PRIMARY KEY,
+  wins smallint NOT NULL CHECK(wins >= 0),
+  losses smallint NOT NULL CHECK(losses >= 0),
+  draws smallint NOT NULL CHECK(draws >= 0),
+  points smallint NOT NULL CHECK(points >= 0)
+
+);

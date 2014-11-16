7ac8ae31a459091532aac5dd13786439a14162d5
Sam Kreter <sakfy6@mail.missouri.edu>
2014-11-16 16:34:30 -0600

    preparing for merge

--- lab10/lab10.sql
+++ lab10/lab10.sql
@@ -13,10 +13,10 @@ CREATE TABLE group_standings(
 
 );
 
- \copy group_standings FROM /facstaff/klaricm/public_cs3380/lab10/lab10_data.csv WITH CSV HEADER
-
-CREATE OR REPLACE FUNCTION
-calc_batting_average_v1(integer, integer)
-RETURNS float AS $$
-SELECT $1 ::float / $2 AS result;
-$$ LANGUAGE SQL;
+--  \copy group_standings FROM /facstaff/klaricm/public_cs3380/lab10/lab10_data.csv WITH CSV HEADER
+--
+-- CREATE OR REPLACE FUNCTION
+-- calc_batting_average_v1(integer, integer)
+-- RETURNS float AS $$
+-- SELECT $1 ::float / $2 AS result;
+-- $$ LANGUAGE SQL;
